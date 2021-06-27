c5c9aa0038865b740a7808b6b04eb50732158123
João Paulo <jptick@gmail.com>
2021-06-27 08:55:36 -0300

    Implementing Method FindById in Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,9 +16,17 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
+
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
     }
 }

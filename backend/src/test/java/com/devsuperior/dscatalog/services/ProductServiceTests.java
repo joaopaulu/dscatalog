61ebdd8bebadb7be6566a95283f85ca71a276d6f
João Paulo <jptick@gmail.com>
2021-07-10 17:51:08 -0300

    Tests Delete ProductService Exceptions

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repository.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,16 +25,40 @@ public class ProductServiceTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
 
         Mockito.doNothing().when(repository).deleteById(existingId);
 
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenIdDoesNotExists(){
+
+        Assertions.assertThrows(DataBaseException.class, () ->{
+            service.delete(dependentId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
+    }
+
+    @Test
+    public void deleteThrowResourceNotFoundExceptionWhenIdDoesNotExists(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () ->{
+            service.delete(nonExistingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
     }
 
     @Test

6d7a5ab79debcfb420f01dca98bc6b56f4f4f065
João Paulo <jptick@gmail.com>
2021-08-12 17:42:41 -0300

    Create ProductDetail Page

---
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -0,0 +1,39 @@
+import ProductPrice from 'components/ProductPrice';
+import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
+const ProductDetails = () => {
+  return (
+    <div className="product-details-container">
+      <div className="product-deails-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+                alt="nome produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={1599.99} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia, quae.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;

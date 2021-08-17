1f80ab0f1245c9948b8a51f74bcdf8e8ef897128
João Paulo <jptick@gmail.com>
2021-08-17 15:03:17 -0300

    Implementing ProductsFilter in Admin

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,28 +1,51 @@
+import { AxiosRequestConfig } from 'axios';
+import ProductFilter, { ProductFilterData } from 'components/ProductFilter';
+import { Product } from 'core/types/product';
+import { SpringPage } from 'core/types/vendor/spring';
+import { requestBackend } from 'core/utils/requests';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCrudCard from '../ProductCardCard';
 import './styles.css';
 
+type ControlComponentsData = {
+  activePage: number;
+  filterData: ProductFilterData;
+};
+
 const List = () => {
-  const product = {
-    id: 1,
-    name: 'The Lord of the Rings',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 90.5,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-    date: '2020-07-13T20:50:07.123450Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Computadores',
-      },
-      {
-        id: 2,
-        name: 'Eletrônicos',
-      },
-    ],
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+      filterData: { name: '', category: null },
+    });
+
+  const handleSubmitFilter = (data: ProductFilterData) => {
+    setControlComponentsData({ activePage: 0, filterData: data });
   };
+
+  const getProducts = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: controlComponentsData.activePage,
+        size: 3,
+        name: controlComponentsData.filterData.name,
+        categoryId: controlComponentsData.filterData.category?.id,
+      },
+    };
+
+    requestBackend(config).then(response => {
+      setPage(response.data);
+    });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
 
   return (
     <div className="product-crud-container">
@@ -32,12 +55,14 @@ const List = () => {
             ADICIONAR
           </button>
         </Link>
-        <div className="base-card product-filter-container">Search bar</div>
+        <ProductFilter onSubmitFilter={handleSubmitFilter} />
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map(product => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
